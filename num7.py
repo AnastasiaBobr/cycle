58f06c91cf925a48fb6404f5931e42cd3acbc90a
AnastasiaBobr <144303478+AnastasiaBobr@users.noreply.github.com>
2023-09-29 20:10:00 +0700

    Update num7.py

--- num7.py
+++ num7.py
@@ -1,9 +1,10 @@
 n = int(input('Введите число:'))
 x = 0
-if n > 0:
-        for i in str(n):
-            if int(i) % 2 == 0:
-                x += int(i)
-        print(x)
-else:
-        print('error')
+while n>0:
+    
+    if n % 2 == 0:
+        x += n % 10
+    n //= 10
+    
+
+print(n)
